7c2d8381b3a7425ce573afdc86242011903045ef
Jerry Zhang <hydrogen.studio.llc@gmail.com>
2023-07-13 09:45:08 +0800

    Optimized Website to allow for normal website publishing

--- renderer/assets/js/theme.js
+++ renderer/assets/js/theme.js
@@ -1,5 +1,11 @@
 // Custom theme code
-
+function openDiscord() {
+    try{
+    require('electron').shell.openExternal('https://discord.gg/RuGAyQ7hqK')
+    }catch(e){
+    window.open('https://discord.gg/RuGAyQ7hqK')
+    }
+}
 
 
 if (document.getElementsByClassName('clean-gallery').length > 0) {
